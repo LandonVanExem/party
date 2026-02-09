31e848a973330851c8103bc8f56958b8951a187e
Landon Van Exem <landon.vanexem@student.thomasmore.be>
2026-02-09 16:05:41 +0100

    Exercise: gebruik een variabele in de template

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,19 +1,28 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"/","/home"})
-    public String home(){
-    return "home";
+    public String home(Model model){
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
+        return "home";
     }
 
 
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        String myName = "Landon Van Exem";
+        String myStreet ="Reeweide 6";
+        String myCity = "Schilde";
+        model.addAttribute("myName", myName);
+        model.addAttribute("myStreet", myStreet);
+        model.addAttribute("myCity", myCity);
         return "about";
     }
 
