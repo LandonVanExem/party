8df27ff3626d3423d8ae0fe01aeafe178167a41b
Landon Van Exem <landon.vanexem@student.thomasmore.be>
2026-02-10 16:08:39 +0100

    data.sql en autogenerated aangevuld

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -2,12 +2,13 @@ package be.thomasmore.party.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Venue {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String venueName;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into venue (venue_name, link_more_info)
+values ('test-venue','test-link');
