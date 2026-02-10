cd9dba42e7931905ee469f99418732b769bd1c15
Landon Van Exem <landon.vanexem@student.thomasmore.be>
2026-02-10 23:23:14 +0100

    VenueController: gebruik de Repository

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,19 +1,33 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.VenueRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    private final VenueRepository venuerepository;
+
+    public VenueController(VenueRepository venuerepository) {
+        this.venuerepository = venuerepository;
+    }
 
     @GetMapping("/venuedetails")
     public String Venue(Model model) {
-        Venue venue1 = new Venue();
-        venue1.setVenueName("the Club");
-        venue1.setLinkMoreInfo("https://theclubmechelen.be/");
-        model.addAttribute("venue1", venue1);
+//        Venue venue = new Venue();
+//        venue.setVenueName("the Club");
+//        venue.setLinkMoreInfo("https://theclubmechelen.be/");
+//        model.addAttribute("venue", venue);
+
+        Optional<Venue> venueFromDb = venuerepository.findById(1);
+        if (venueFromDb.isPresent()){
+            model.addAttribute("venue",venueFromDb.get());
+        }
+
         return "venuedetails";
     }
 }
