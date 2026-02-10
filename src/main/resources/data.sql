17da6542266bb587879877ffc294fc692161c4a0
Landon Van Exem <landon.vanexem@student.thomasmore.be>
2026-02-11 00:03:56 +0100

    Exercise: Herhaling gebruik Venue object in Thymeleaf

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into venue (venue_name, link_more_info)
-values ('the club','club-link');
+insert into venue (venue_name, link_more_info, capacity, food_provided, indoor, outdoor,
+                   free_parking_available, city, distance_from_public_transport_in_km)
+values ('De Club','https://transitm.mechelen.be/de-club',150, false,true,false,
+        true,'Mechelen',2);
