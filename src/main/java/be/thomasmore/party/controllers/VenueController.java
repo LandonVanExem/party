567e2ad2fdae180489180f1a99cf0b4acbf86e44
Landon Van Exem <landon.vanexem@student.thomasmore.be>
2026-02-10 14:39:41 +0100

    Een Venue Object

---
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -0,0 +1,19 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Venue;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VenueController {
+
+    @GetMapping("/venuedetails")
+    public String Venue(Model model){
+        Venue venue1= new Venue();
+        venue1.setVenueName("the Club");
+        venue1.setLinkMoreInfo("local bar");
+        model.addAttribute("venue1",venue1);
+        return "venuedetails";
+    }
+}
